f021050278299a8db18b990f0d935c34331d64ea
nodes777 <nodes777@gmail.com>
2016-08-21 10:23:56 -0700

    changes from and subject lines in php

--- index.php
+++ index.php
@@ -2,19 +2,19 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $from = 'From: TangledDemo'; 
-    $to = 'jeff@spreadthevibes.org'; 
-    $subject = 'Hello';
+    $from = 'From: Landing Page Demo';
+    $to = 'jeff@spreadthevibes.org';
+    $subject = 'From the Landing Page';
     $human = $_POST['human'];
-			
+
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-				
-    if ($_POST['submit'] && $human == '4') {				 
-        if (mail ($to, $subject, $body, $from)) { 
+
+    if ($_POST['submit'] && $human == '4') {
+        if (mail ($to, $subject, $body, $from)) {
 	    echo '<p>Your message has been sent!</p>';
-	} else { 
-	    echo '<p>Something went wrong, go back and try again!</p>'; 
-	} 
+	} else {
+	    echo '<p>Something went wrong, go back and try again!</p>';
+	}
     } else if ($_POST['submit'] && $human != '4') {
 	echo '<p>You answered the anti-spam question incorrectly!</p>';
     }
